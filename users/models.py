5301fee66f2842d3289201197cb1516314738f40
Yaman-Katalan <23040456@student.ltuc.com>
2024-08-03 18:14:38 +0000

    abc

---
+++ users/models.py
@@ -0,0 +1,10 @@
+# users/models.py
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    pass
+
+    def __str__(self):
+        return self.username
